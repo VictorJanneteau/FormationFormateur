2cb784c4188d7f5699c1013030c2314e6a8b52d6
Helena <Helena.adi@live.fr>
2020-11-22 17:34:15 +0100

    ajoutEnfant

---
+++ ajoutEnfant.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE HTML>
+<html>
+    <body>
+    <?php
+        include("bdd.php");
+
+        if(isset($_POST['boutonEnfantAjout']))
+        {
+            $nomEnfant=$_POST['nomEnfant'];
+            $prenomEnfant=$_POST['prenomEnfant'];
+            $ageEnfant=$_POST['ageEnfant'];
+            $villeEnfant=$_POST['villeEnfant'];
+
+            $ajoutEnfant = $connect->prepare('INSERT INTO ENFANTS (prenomEnfant,nomEnfant,ageEnfant,villeEnfant) 
+            VALUES(:nomEnfant, :prenomEnfant, :ageEnfant, :villeEnfant)');
+            $ajoutEnfant->bindValue(":nomEnfant",$nomEnfant, PDO::PARAM_STR);
+            $ajoutEnfant->bindValue(":prenomEnfant",$prenomEnfant, PDO::PARAM_STR);
+            $ajoutEnfant->bindValue(":ageEnfant",$ageEnfant, PDO::PARAM_STR);
+            $ajoutEnfant->bindValue(":villeEnfant",$villeEnfant, PDO::PARAM_STR);
+            $ajoutEnfant->execute();
+        }
+
+    ?>
+
+</body> 
+</html> 
