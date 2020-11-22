9fc9cd73640286ca40da1abd292e47203cf16d00
Victor Janneteau <victor.janneteau@laposte.net>
2020-11-22 17:43:43 +0100

    bdd

--- ajoutEnfant.php
+++ ajoutEnfant.php
@@ -12,7 +12,7 @@
             $ageEnfant=$_POST['ageEnfant'];
             $villeEnfant=$_POST['villeEnfant'];
 
-            $ajoutEnfant = $connect->prepare('INSERT INTO ENFANTS (prenomEnfant,nomEnfant,ageEnfant,villeEnfant) 
+            $ajoutEnfant = $bdd->prepare('INSERT INTO PARTIEB__ENFANTS (prenomEnfant,nomEnfant,ageEnfant,villeEnfant) 
             VALUES(:nomEnfant, :prenomEnfant, :ageEnfant, :villeEnfant)');
             $ajoutEnfant->bindValue(":nomEnfant",$nomEnfant, PDO::PARAM_STR);
             $ajoutEnfant->bindValue(":prenomEnfant",$prenomEnfant, PDO::PARAM_STR);
